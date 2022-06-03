20f84247d728a4c7fc3d48a9a8026d406382bb25
Alex Balogh <baloghaj@mail.uc.edu>
2022-06-03 12:55:33 -0400

    Updated JS

--- resources/js/index.js
+++ resources/js/index.js
@@ -12,9 +12,6 @@ async function updateMessages() {
       formattedMessages += formatMessage(message, nameInput.value);
   });
   chatBox.innerHTML = formattedMessages;
-  updateMessages()
-  const MILLISECONDS_IN_TEN_SECONDS = 10000;
-  setInterval(updateMessages, MILLISECONDS_IN_TEN_SECONDS);
 }
 
 const serverURL = `https://it3049c-chat-application.herokuapp.com/messages`;
@@ -24,41 +21,45 @@ function fetchMessages() {
     .then( response => response.json())
   }
 
-  function formatMessage(message, myNameInput) {
-    const time = new Date(message.timestamp);
-    const formattedTime = `${time.getHours()}:${time.getMinutes()}`;
+function formatMessage(message, myNameInput) {
+  const time = new Date(message.timestamp);
+  const formattedTime = `${time.getHours()}:${time.getMinutes()}`;
 
-    if (myNameInput === message.sender) {
-        return `
-        <div class="mine messages">
-            <div class="message">
-                ${message.text}
-            </div>
-            <div class="sender-info">
-                ${formattedTime}
-            </div>
+  if (myNameInput === message.sender) {
+      return `
+      <div class="mine messages">
+        <div class="message">
+          ${message.text}
         </div>
-        `
-    } else {
-        return `
-            <div class="yours messages">
-                <div class="message">
-                    ${message.text}
-                </div>
-                <div class="sender-info">
-                    ${message.sender} ${formattedTime}
-                </div>
-            </div>
-        `
-    }
+        <div class="sender-info">
+          ${formattedTime}
+        </div>
+      </div>
+      `
+  } 
+  else {
+    return `
+      <div class="yours messages">
+        <div class="message">
+          ${message.text}
+        </div>
+        <div class="sender-info">
+          ${message.sender} ${formattedTime}
+        </div>
+      </div>
+    `
+  }
 }
+
+const MILLISECONDS_IN_TEN_SECONDS = 10000;
+setInterval(updateMessages, MILLISECONDS_IN_TEN_SECONDS);
 
 //Sending Messages
 function sendMessages(username, text) {
   const newMessage = {
-      sender: username,
-      text: text,
-      timestamp: new Date()
+    sender: username,
+    text: text,
+    timestamp: new Date()
   }
 
   fetch (serverURL, {
@@ -69,6 +70,7 @@ function sendMessages(username, text) {
       body: JSON.stringify(newMessage)
   });
 }
+
 sendButton.addEventListener("click", function(sendButtonClickEvent) {
   sendButtonClickEvent.preventDefault();
   const sender = nameInput.value;
